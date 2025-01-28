f68e42b0a85004d3030b015adc60bbe1d7bd0774
Brandon Rodriguez <brodriguez8774@gmail.com>
2025-01-28 17:59:23 -0500

    Split constants into separate files for organization

---
+++ adminlte2_pdq/constants/__init__.py
@@ -0,0 +1,51 @@
+"""
+Imports to make this folder behave like a single file.
+"""
+
+# Constants related to general package settings.
+from .general_constants import (
+    DATETIME_WIDGET,
+    DATE_WIDGET,
+    TIME_WIDGET,
+    BOLD_REQUIRED_FIELDS,
+    ASTERISK_REQUIRED_FIELDS,
+    # Message settings.
+    RESPONSE_403_DEBUG_MESSAGE,
+    RESPONSE_403_PRODUCTION_MESSAGE,
+    RESPONSE_404_DEBUG_MESSAGE,
+    RESPONSE_404_PRODUCTION_MESSAGE,
+    # Debug text settings.
+    TEXT_BLUE,
+    TEXT_CYAN,
+    TEXT_GREEN,
+    TEXT_PURPLE,
+    TEXT_RED,
+    TEXT_RESET,
+    TEXT_YELLOW,
+)
+
+
+# Constants related to package routes.
+from .route_and_policy_constants import (
+    LOGIN_URL,
+    LOGOUT_ROUTE,
+    PWD_RESET_ROUTE,
+    PWD_RESET_DONE_ROUTE,
+    PWD_RESET_CONFIRM_ROUTE,
+    PWD_RESET_COMPLETE_ROUTE,
+    REGISTER_ROUTE,
+    MEDIA_ROUTE,
+    WEBSOCKET_ROUTE,
+    HOME_ROUTE,
+    PWD_CHANGE,
+    PWD_CHANGE_DONE,
+)
+
+
+# Constants related to package policy.
+from .route_and_policy_constants import (
+    LOGIN_EXEMPT_WHITELIST,
+    STRICT_POLICY_WHITELIST,
+    LOGIN_REQUIRED,
+    STRICT_POLICY,
+)
