5f564f9d8159d2ab8e80aaf3023d031a36c501f6
Brandon Rodriguez <brodriguez8774@gmail.com>
2025-02-15 15:42:24 -0500

    Rework existing demo css content into multiple pages

--- adminlte2_pdq/urls.py
+++ adminlte2_pdq/urls.py
@@ -4,7 +4,7 @@ Django AdminLTE2 Default URL Configuration
 
 # Third-Party Imports.
 from django.conf import settings
-from django.urls import path
+from django.urls import include, path
 from django.views.generic import RedirectView
 
 # Internal Imports.
@@ -13,16 +13,30 @@ from . import views
 
 app_name = "adminlte2_pdq"
 urlpatterns = [
-    # Sample pages
+    # Sample pages.
     path("home/", views.home, name="home"),
     path("accounts/register/", views.register, name="register"),
     path("sample_form/", views.sample_form, name="sample_form"),
     path("sample1/", views.sample1, name="sample1"),
     path("sample2/", views.sample2, name="sample2"),
-    path("demo-css/", views.demo_css, name="demo-css"),
+    # Demo CSS pages.
+    path(
+        "demo-css",
+        include(
+            [
+                path("alerts/", views.demo_css_alerts, name="demo-css-alerts"),
+                path("boxes/", views.demo_css_boxes, name="demo-css-boxes"),
+                path("buttons/", views.demo_css_buttons, name="demo-css-buttons"),
+                path("labels/", views.demo_css_labels, name="demo-css-labels"),
+                path("modals/", views.demo_css_modals, name="demo-css-modals"),
+                path("tables/", views.demo_css_tables, name="demo-css-tables"),
+                path("", views.demo_css, name="demo-css"),
+            ]
+        ),
+    ),
     # 404 view. Required to make some logic importing behave as expected.
     path("view-404/", views.view_404, name="view-404"),
-    # Redirects to the home page
+    # Redirects to the home page.
     path(
         "",
         RedirectView.as_view(
