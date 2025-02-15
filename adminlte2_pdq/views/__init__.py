f5eae247f3fb3e70273e58ccc8a237c5b8822161
Brandon Rodriguez <brodriguez8774@gmail.com>
2025-02-15 16:40:17 -0500

    Further rework demo_css page file structure for future maintainability

---
+++ adminlte2_pdq/views/__init__.py
@@ -0,0 +1,25 @@
+"""
+Imports view logic for "views" folder.
+Makes project imports to this folder behave like a standard single file.
+"""
+
+# General view imports.
+from .general import (
+    home,
+    register,
+    sample_form,
+    sample1,
+    sample2,
+    view_404,
+)
+
+# Demo css view imports.
+from .demo_css import (
+    demo_css,
+    demo_css_alerts,
+    demo_css_boxes,
+    demo_css_buttons,
+    demo_css_labels,
+    demo_css_modals,
+    demo_css_tables,
+)
