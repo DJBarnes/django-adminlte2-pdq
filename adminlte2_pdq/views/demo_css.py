f36c66b0a74d4cdd10a3f9982e15570d452a798b
Brandon Rodriguez <brodriguez8774@gmail.com>
2025-02-15 16:40:17 -0500

    Further rework demo_css page file structure for future maintainability

--- adminlte2_pdq/views/demo_css.py
+++ adminlte2_pdq/views/demo_css.py
@@ -4,19 +4,10 @@
 import logging
 
 # Third-Party Imports.
-from django.conf import settings
 from django.contrib import messages
-from django.shortcuts import redirect, render
+from django.shortcuts import render
 
-# Internal Imports.
-from .constants.general_constants import RESPONSE_404_DEBUG_MESSAGE, RESPONSE_404_PRODUCTION_MESSAGE
-from .decorators import (
-    login_required,
-    permission_required,
-    permission_required_one,
-)
-from .forms import SampleForm, SampleFormset
-from .menu import CSS_MENU
+from adminlte2_pdq.menu import CSS_MENU
 
 
 # Initialize logger.
@@ -45,68 +36,6 @@ BOOTSTRAP_TYPES = [
 ]
 
 
-def home(request):
-    """Show default home page"""
-    return render(request, "adminlte2/home.html", {})
-
-
-def register(request):
-    """Show default register page"""
-    dummy_form = {
-        "errors": None,
-        "non_field_errors": None,
-    }
-    return render(
-        request,
-        "registration/register.html",
-        {
-            "form": dummy_form,
-        },
-    )
-
-
-@login_required()
-def sample_form(request):
-    """Show sample form page"""
-    form = SampleForm()
-    form["sample_phone"].phone_info = {
-        "pattern": r"\([0-9]{3}\) [0-9]{3}-[0-9]{4}",
-        "inputmask": "(999) 999-9999",
-    }
-    form["sample_range"].range_min_max = {
-        "min": 5,
-        "max": 10,
-    }
-    form["sample_color"].is_color = True
-    form["sample_datalist"].datalist = {
-        "name": "my_awesome_datalist",
-        "data": ("foo", "bar"),
-    }
-
-    formset = SampleFormset()
-
-    return render(
-        request,
-        "adminlte2/sample_form.html",
-        {"form": form, "formset": formset},
-    )
-
-
-@permission_required(["auth.add_group", "auth.change_group", "auth.delete_group"])
-def sample1(request):
-    """Show default sample1 page"""
-    return render(request, "adminlte2/sample1.html", {})
-
-
-@permission_required_one(["auth.add_permission", "auth.change_permission", "auth.delete_permission"])
-def sample2(request):
-    """Show default sample2 page"""
-    return render(request, "adminlte2/sample2.html", {})
-
-
-# region Demo CSS
-
-
 def demo_css(request):
     """Show examples of extra-features.css"""
 
@@ -121,7 +50,7 @@ def demo_css(request):
 
     return render(
         request,
-        "adminlte2/demo_css/home.html",
+        "adminlte2_demo_css/old/home.html",
         {
             "bootstrap_types": BOOTSTRAP_TYPES,
             "ADMINLTE2_MENU": CSS_MENU,
@@ -143,7 +72,7 @@ def demo_css_alerts(request):
 
     return render(
         request,
-        "adminlte2/demo_css/alerts.html",
+        "adminlte2_demo_css/old/alerts.html",
         {
             "bootstrap_types": BOOTSTRAP_TYPES,
             "ADMINLTE2_MENU": CSS_MENU,
@@ -156,7 +85,7 @@ def demo_css_boxes(request):
 
     return render(
         request,
-        "adminlte2/demo_css/boxes.html",
+        "adminlte2_demo_css/old/boxes.html",
         {
             "bootstrap_types": BOOTSTRAP_TYPES,
             "ADMINLTE2_MENU": CSS_MENU,
@@ -169,7 +98,7 @@ def demo_css_buttons(request):
 
     return render(
         request,
-        "adminlte2/demo_css/buttons.html",
+        "adminlte2_demo_css/old/buttons.html",
         {
             "bootstrap_types": BOOTSTRAP_TYPES,
             "ADMINLTE2_MENU": CSS_MENU,
@@ -182,7 +111,7 @@ def demo_css_labels(request):
 
     return render(
         request,
-        "adminlte2/demo_css/labels.html",
+        "adminlte2_demo_css/old/labels.html",
         {
             "bootstrap_types": BOOTSTRAP_TYPES,
             "ADMINLTE2_MENU": CSS_MENU,
@@ -195,7 +124,7 @@ def demo_css_modals(request):
 
     return render(
         request,
-        "adminlte2/demo_css/modals.html",
+        "adminlte2_demo_css/old/modals.html",
         {
             "bootstrap_types": BOOTSTRAP_TYPES,
             "ADMINLTE2_MENU": CSS_MENU,
@@ -208,7 +137,7 @@ def demo_css_tables(request):
 
     return render(
         request,
-        "adminlte2/demo_css/tables.html",
+        "adminlte2_demo_css/old/tables.html",
         {
             "bootstrap_types": BOOTSTRAP_TYPES,
             "ADMINLTE2_MENU": CSS_MENU,
@@ -217,24 +146,3 @@ def demo_css_tables(request):
 
 
 # endregion Demo CSS
-
-
-def view_404(request, exception):
-    """On failure to locate a route, redirect to home page."""
-
-    # Display warning.
-    if settings.DEBUG:
-        # Handle output when DEBUG = True.
-        if len(RESPONSE_404_DEBUG_MESSAGE) > 0:
-            messages.warning(request, RESPONSE_404_DEBUG_MESSAGE)
-            logger.warning(RESPONSE_404_DEBUG_MESSAGE)
-            messages.debug(request, str(exception))
-            logger.warning(str(exception))
-    else:
-        # Handle output when DEBUG = False.
-        if len(RESPONSE_404_PRODUCTION_MESSAGE) > 0:
-            messages.warning(request, RESPONSE_404_PRODUCTION_MESSAGE)
-
-    # Redirect to home.
-    home_route = getattr(settings, "ADMINLTE2_HOME_ROUTE", "adminlte2_pdq:home")
-    return redirect(home_route)
