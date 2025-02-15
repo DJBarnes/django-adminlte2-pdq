4143ff1db8d6b9002cdf5b0a4ddc618d0a73f5c0
Brandon Rodriguez <brodriguez8774@gmail.com>
2025-02-15 15:42:24 -0500

    Rework existing demo css content into multiple pages

--- adminlte2_pdq/menu.py
+++ adminlte2_pdq/menu.py
@@ -61,3 +61,94 @@ MENU = [
         ],
     },
 ]
+
+
+# Used only on the demo-css pages.
+CSS_MENU = [
+    {
+        "text": "Home",
+        "nodes": [
+            {
+                "route": getattr(settings, "ADMINLTE2_HOME_ROUTE", "adminlte2_pdq:home"),
+                "text": "Home",
+                "icon": "fa fa-dashboard",
+            },
+            {
+                "text": "Demo CSS",
+                "nodes": [
+                    {
+                        "route": "adminlte2_pdq:demo-css",
+                        "text": "Demo CSS | Home",
+                        "icon": "fa fa-home",
+                    },
+                    {
+                        "route": "adminlte2_pdq:demo-css-alerts",
+                        "text": "Demo CSS | Alerts",
+                        "icon": "fa fa-exclamation",
+                    },
+                    {
+                        "route": "adminlte2_pdq:demo-css-boxes",
+                        "text": "Demo CSS | Boxes",
+                        "icon": "fa fa-square",
+                    },
+                    {
+                        "route": "adminlte2_pdq:demo-css-buttons",
+                        "text": "Demo CSS | Buttons",
+                        "icon": "fa fa-square-o",
+                    },
+                    {
+                        "route": "adminlte2_pdq:demo-css-labels",
+                        "text": "Demo CSS | Labels",
+                        "icon": "fa fa-tags",
+                    },
+                    {
+                        "route": "adminlte2_pdq:demo-css-modals",
+                        "text": "Demo CSS | Modals",
+                        "icon": "fa fa-comment",
+                    },
+                    {
+                        "route": "adminlte2_pdq:demo-css-tables",
+                        "text": "Demo CSS | Tables",
+                        "icon": "fa fa-table",
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        "text": "Profile",
+        "nodes": [
+            {
+                "route": "password_change",
+                "text": "Change Password",
+                "icon": "fa fa-lock",
+            }
+        ],
+    },
+    {
+        "text": "Samples",
+        "nodes": [
+            {
+                "route": "adminlte2_pdq:sample_form",
+                "text": "Sample Form",
+                "icon": "fa fa-list-alt",
+            },
+            {
+                "route": "adminlte2_pdq:sample1",
+                "text": "Sample1",
+                "icon": "fa fa-group",
+            },
+            {
+                "text": "Sample Tree",
+                "icon": "fa fa-leaf",
+                "nodes": [
+                    {
+                        "route": "adminlte2_pdq:sample2",
+                        "text": "Sample2",
+                        "icon": "fa fa-building",
+                    },
+                ],
+            },
+        ],
+    },
+]
--- adminlte2_pdq/views.py
+++ adminlte2_pdq/views.py
@@ -4,20 +4,45 @@
 import logging
 
 # Third-Party Imports.
+from django.conf import settings
 from django.contrib import messages
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Internal Imports.
+from .constants.general_constants import RESPONSE_404_DEBUG_MESSAGE, RESPONSE_404_PRODUCTION_MESSAGE
 from .decorators import (
     login_required,
     permission_required,
     permission_required_one,
 )
 from .forms import SampleForm, SampleFormset
+from .menu import CSS_MENU
 
 
 # Initialize logger.
 logger = logging.getLogger(__name__)
+
+
+# Module level variables.
+BOOTSTRAP_TYPES = [
+    "default",
+    "primary",
+    "info",
+    "success",
+    "warning",
+    "danger",
+    "navy",
+    "teal",
+    "olive",
+    "lime",
+    "orange",
+    "fuchsia",
+    "indigo",
+    "purple",
+    "maroon",
+    "gray",
+    "black",
+]
 
 
 def home(request):
@@ -79,42 +104,137 @@ def sample2(request):
     return render(request, "adminlte2/sample2.html", {})
 
 
+# region Demo CSS
+
+
 def demo_css(request):
     """Show examples of extra-features.css"""
 
     # Add messages to demo them.
     messages.set_level(request, messages.DEBUG)
-    messages.debug(request, "This is a debug message via the messages framework")
-    messages.info(request, "This is a info message via the messages framework")
-    messages.success(request, "This is a success message via the messages framework")
-    messages.warning(request, "This is a warning message via the messages framework")
-    messages.error(request, "This is a error message via the messages framework")
-    messages.add_message(request, 50, "This is an unknown level message via the messages framework")
+    messages.debug(request, "This is a debug message via the messages framework.")
+    messages.info(request, "This is a info message via the messages framework.")
+    messages.success(request, "This is a success message via the messages framework.")
+    messages.warning(request, "This is a warning message via the messages framework.")
+    messages.error(request, "This is a error message via the messages framework.")
+    messages.add_message(request, 50, "This is an unknown level message via the messages framework.")
 
-    # Define the bootstrap "colors" to demo.
-    bootstrap_types = [
-        "default",
-        "primary",
-        "info",
-        "success",
-        "warning",
-        "danger",
-        "navy",
-        "teal",
-        "olive",
-        "lime",
-        "orange",
-        "fuchsia",
-        "indigo",
-        "purple",
-        "maroon",
-        "gray",
-        "black",
-    ]
     return render(
         request,
-        "adminlte2/demo_css.html",
+        "adminlte2/demo_css/home.html",
         {
-            "bootstrap_types": bootstrap_types,
+            "bootstrap_types": BOOTSTRAP_TYPES,
+            "ADMINLTE2_MENU": CSS_MENU,
         },
     )
+
+
+def demo_css_alerts(request):
+    """Show example alert elements."""
+
+    # Add messages to demo them.
+    messages.set_level(request, messages.DEBUG)
+    messages.debug(request, "This is a debug message via the messages framework.")
+    messages.info(request, "This is a info message via the messages framework.")
+    messages.success(request, "This is a success message via the messages framework.")
+    messages.warning(request, "This is a warning message via the messages framework.")
+    messages.error(request, "This is a error message via the messages framework.")
+    messages.add_message(request, 50, "This is an unknown level message via the messages framework.")
+
+    return render(
+        request,
+        "adminlte2/demo_css/alerts.html",
+        {
+            "bootstrap_types": BOOTSTRAP_TYPES,
+            "ADMINLTE2_MENU": CSS_MENU,
+        },
+    )
+
+
+def demo_css_boxes(request):
+    """Show example box elements."""
+
+    return render(
+        request,
+        "adminlte2/demo_css/boxes.html",
+        {
+            "bootstrap_types": BOOTSTRAP_TYPES,
+            "ADMINLTE2_MENU": CSS_MENU,
+        },
+    )
+
+
+def demo_css_buttons(request):
+    """Show example button elements."""
+
+    return render(
+        request,
+        "adminlte2/demo_css/buttons.html",
+        {
+            "bootstrap_types": BOOTSTRAP_TYPES,
+            "ADMINLTE2_MENU": CSS_MENU,
+        },
+    )
+
+
+def demo_css_labels(request):
+    """Show example label elements."""
+
+    return render(
+        request,
+        "adminlte2/demo_css/labels.html",
+        {
+            "bootstrap_types": BOOTSTRAP_TYPES,
+            "ADMINLTE2_MENU": CSS_MENU,
+        },
+    )
+
+
+def demo_css_modals(request):
+    """Show example modal elements."""
+
+    return render(
+        request,
+        "adminlte2/demo_css/modals.html",
+        {
+            "bootstrap_types": BOOTSTRAP_TYPES,
+            "ADMINLTE2_MENU": CSS_MENU,
+        },
+    )
+
+
+def demo_css_tables(request):
+    """Show example table elements."""
+
+    return render(
+        request,
+        "adminlte2/demo_css/tables.html",
+        {
+            "bootstrap_types": BOOTSTRAP_TYPES,
+            "ADMINLTE2_MENU": CSS_MENU,
+        },
+    )
+
+
+# endregion Demo CSS
+
+
+def view_404(request, exception):
+    """On failure to locate a route, redirect to home page."""
+
+    # Display warning.
+    if settings.DEBUG:
+        # Handle output when DEBUG = True.
+        if len(RESPONSE_404_DEBUG_MESSAGE) > 0:
+            messages.warning(request, RESPONSE_404_DEBUG_MESSAGE)
+            logger.warning(RESPONSE_404_DEBUG_MESSAGE)
+            messages.debug(request, str(exception))
+            logger.warning(str(exception))
+    else:
+        # Handle output when DEBUG = False.
+        if len(RESPONSE_404_PRODUCTION_MESSAGE) > 0:
+            messages.warning(request, RESPONSE_404_PRODUCTION_MESSAGE)
+
+    # Redirect to home.
+    home_route = getattr(settings, "ADMINLTE2_HOME_ROUTE", "adminlte2_pdq:home")
+    return redirect(home_route)
