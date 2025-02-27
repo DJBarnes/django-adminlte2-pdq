c54efd59bfa78029ad72190d5ed2aec4ef4d2296
Brandon Rodriguez <brodriguez8774@gmail.com>
2025-02-27 03:11:44 -0500

    Split up custom css and js into multiple files, for organization

---
+++ adminlte2_pdq/static/adminlte2_pdq/js/conflicts/conflicts.js
@@ -0,0 +1,10 @@
+/**
+ * Handling for JavaScript conflicts within existing/required libraries.
+ *
+ * For package clarity and organization, this file only imports other files.
+ * All logic should be split into the `partials` subfolder.
+ */
+
+
+import "./partials/ui_tooltip_conflict_fix.js";
+import "./partials/header_breadcrumb_overlap_fix.js";
