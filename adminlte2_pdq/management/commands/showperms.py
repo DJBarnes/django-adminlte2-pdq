603cfbb2b6746ef1facc167d4bfc8db863bdfefb
David Barnes <barnesdavidj@gmail.com>
2025-07-07 13:36:19 -0400

    Add column to showperms command to show the "has_perm" text that must be used. Add option to hide the "has_perm" column. Add option to remove all of the default perms that Django creates automatically from the output.

--- adminlte2_pdq/management/commands/showperms.py
+++ adminlte2_pdq/management/commands/showperms.py
@@ -5,13 +5,21 @@ Command to list all permissions.
 # Third-Party Imports.
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import Permission
-from django.db.models import F
+from django.db.models import F, Value
+from django.db.models.functions import Concat
 
 
 class Command(BaseCommand):
     """Command to list out all project permissions."""
 
     help = "List out all of the permissions in the database."
+
+    DEFAULT_PERMS = (
+        "add_",
+        "change_",
+        "delete_",
+        "view_",
+    )
 
     def add_arguments(self, parser):
         """Define arguments to pass into command."""
@@ -32,7 +40,23 @@ class Command(BaseCommand):
             "--hide-content-types",
             default=False,
             action="store_true",
-            help='Skip displaying "content type" value for each permission.',
+            help='Skip displaying the "content type" value for each permission.',
+        )
+        parser.add_argument(
+            "--hide-has_perm-text",
+            default=False,
+            action="store_true",
+            help='Skip displaying the "has_perm" value for each permission.',
+        )
+        parser.add_argument(
+            "--hide-default-perms",
+            default=False,
+            action="store_true",
+            help=(
+                "Skip displaying the default perms that every model has. "
+                "EX: add_<MODEL>, change_<MODEL>, delete_<MODEL>, view_<MODEL> "
+                'NOTE: This option uses "starts_with" to omit perms and could hide others that start the same way.'
+            ),
         )
 
     def handle(self, *args, **options):
@@ -44,6 +68,7 @@ class Command(BaseCommand):
             "name_len": 0,
             "codename_len": 0,
             "content_type_len": 0,
+            "has_perm_len": 0,
         }
 
         # Handle permission "name" arg.
@@ -55,6 +80,12 @@ class Command(BaseCommand):
         # Handle permission "content type" arg.
         include_content_types = not options["hide_content_types"]
 
+        # Handle permission default arg.
+        include_default_types = not options["hide_default_perms"]
+
+        # Handle permission has_perm text.
+        include_has_perm_text = not options["hide_has_perm_text"]
+
         # Get all permission objects.
         # all_permissions = Permission.objects.all()
         all_permissions = Permission.objects.order_by(
@@ -64,6 +95,7 @@ class Command(BaseCommand):
             "name",
             "codename",
             content_type_label=F("content_type__app_label"),
+            has_perm_label=Concat("content_type__app_label", Value("."), "codename"),
         )
 
         # Update calculated max lengths.
@@ -71,12 +103,14 @@ class Command(BaseCommand):
             max_lengths["name_len"] = max(max_lengths["name_len"], len(perm["name"]))
             max_lengths["codename_len"] = max(max_lengths["codename_len"], len(perm["codename"]))
             max_lengths["content_type_len"] = max(max_lengths["content_type_len"], len(perm["content_type_label"]))
+            max_lengths["has_perm_len"] = max(max_lengths["has_perm_len"], len(perm["has_perm_label"]))
 
         # Calculate separator and header strings.
         header_str, separator_str, max_lengths = self.process_header_str(
             include_names,
             include_codenames,
             include_content_types,
+            include_has_perm_text,
             max_lengths,
         )
 
@@ -87,6 +121,10 @@ class Command(BaseCommand):
 
         current_content_type = ""
         for perm in all_permissions:
+
+            # Check to see if we should skip the built-in default perms
+            if not include_default_types and perm["codename"].startswith(self.DEFAULT_PERMS):
+                continue
 
             # Handle if con tent type changed.
             if current_content_type == "":
@@ -105,6 +143,9 @@ class Command(BaseCommand):
             if include_codenames:
                 line += "| {codename:{codename_len}} "
 
+            if include_has_perm_text:
+                line += "| {has_perm_label:{has_perm_len}} "
+
             line += "|"
 
             # Print parsed output values to console.
@@ -118,6 +159,7 @@ class Command(BaseCommand):
         include_names,
         include_codenames,
         include_content_types,
+        include_has_perm_text,
         max_lengths,
     ):
         """Calculates header string and separator string values for nicer output."""
@@ -159,6 +201,19 @@ class Command(BaseCommand):
                 separator_str += "-+-"
             header_str += header_value.ljust(max_lengths["codename_len"])
             separator_str += "-" * max_lengths["codename_len"]
+
+        # Handle "has_perm" value.
+        if include_has_perm_text:
+            header_value = "has_perm"
+            max_lengths["has_perm_len"] = max(max_lengths["has_perm_len"], len(header_value))
+            if separator_str == "":
+                header_str += "| "
+                separator_str += "+-"
+            else:
+                header_str += " | "
+                separator_str += "-+-"
+            header_str += header_value.ljust(max_lengths["has_perm_len"])
+            separator_str += "-" * max_lengths["has_perm_len"]
 
         header_str += " |"
         separator_str += "-+"
